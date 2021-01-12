dd6b5f6d9c4f3f30213c4bbd08d6596cac735af6
Louise <louisepeiffer@hotmail.com>
2021-01-12 17:01:26 +0100

    tout sauf exo 9

---
+++ public/js/main.js
@@ -0,0 +1,67 @@
+// Exo 1
+let addition = (a,b) => {
+    return a + b
+}
+
+let nb = addition(10, 5)
+console.log(nb);
+
+// Exo 2
+let sous = (c,d) => {
+    return c-d
+}
+
+let sousRes = sous(10, 5)
+console.log(sousRes);
+
+// Exo 3
+let mult = (e,f) => {
+    return e*f
+}
+
+let multip = mult(10, 5)
+console.log(multip);
+
+// Exo 4
+let div = (g,h) => {
+    return g/h
+}
+
+let division = div(10, 5)
+console.log(division);
+
+// Exo 5
+let fonc = (el1, el2) => {
+    return el1%el2
+}
+
+let foncDivRest = fonc(40, 17)
+console.log(foncDivRest);
+
+// Exo 6
+let carre = (coucou) => {
+    return coucou*coucou
+}
+
+let ress = carre(10)
+console.log(ress);
+
+// Exo 7
+let expo = (salut, bitch) => {
+    return Math.pow(salut, bitch)
+}
+
+let etDoonc = expo(3,6)
+console.log(etDoonc);
+
+// Exo 8
+let cap = (hey) => {
+    return hey.charAt(0).toUpperCase() + hey.slice(1)
+}
+let dascap = cap ("salut")
+console.log(dascap);
+
+// Exo 9
+let calcul = (nbr1 , operator , nbr2) => {
+    
+}
